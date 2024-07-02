cf968fde4e8defe4808bf54207bc9ef89cf29eef
Chandudixitt <yelheriv@gmail.com>
2024-07-02 14:47:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "subscription_id" {
-  description = "The subscription ID for Azure"
-  type        = string
-}
-
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
@@ -34,12 +29,23 @@ variable "subnet_bastion_address_prefix" {
 }
 
 variable "storage_account_name" {
-  description = "The location where the resource group will be created"
+  description = "The name of the storage account"
   type        = string
 }
 
-variable "storage_container_name" {
-  description = "The location where the resource group will be created"
+variable "storage_account_tier" {
+  description = "The tier of the storage account"
   type        = string
+  default     = "Standard"
 }
 
+variable "storage_container_names" {
+  description = "A list of container names to create within the storage account."
+  type        = list(string)
+}
+
+variable "storage_account_replication_type" {
+  description = "The replication type of the storage account"
+  type        = string
+  default     = "LRS"
+}
